db6d25dae742cceeaff9473df6222221f544e963
Brandon Dyer-Smith <brandon.dyer-smith@nologostudios.com>
2025-09-11 15:35:55 +0200

    Added more to GK sql

--- SQL/GK Stat Normalisation and Analysis.sql
+++ SQL/GK Stat Normalisation and Analysis.sql
@@ -14,10 +14,16 @@ SELECT
 	Squad,
 	Pos1,
 	Minutes,
+	MP,
+	Nineties,
 	(CAST(GA AS float) - MAX(GA) OVER())/ NULLIF(MIN(GA) OVER () - MAX(GA) OVER(), 0) AS norm_GoalsAllowed,
 	CAST(1 - (CAST(PSxG AS decimal(10,2)) - MIN(PSxG) OVER()) / NULLIF(MAX(PSxG) OVER() - MIN(PSxG) OVER(), 0)AS decimal(6,3)) AS norm_PSxG,
 	CAST((CAST(PSxGallowed90 AS decimal(10,2)) - MIN(PSxGallowed90) OVER()) / NULLIF(MAX(PSxGallowed90) OVER() - MIN(PSxGallowed90) OVER(), 0) as decimal(6,3)) as norm_PSxGallowed90,
-	CAST((CAST(PSxGperShotonTarget AS decimal(10,2)) - MIN(PSxGperShotonTarget) OVER()) / NULLIF(MAX(PSxGperShotonTarget) OVER() - MIN(PSxGperShotonTarget) OVER(), 0) as decimal(6,3)) as norm_PSxGperShotonTarget
+	CAST((CAST(PSxGperShotonTarget AS decimal(10,2)) - MIN(PSxGperShotonTarget) OVER()) / NULLIF(MAX(PSxGperShotonTarget) OVER() - MIN(PSxGperShotonTarget) OVER(), 0) as decimal(6,3)) as norm_PSxGperShotonTarget,
+	CAST((CAST(PSxGminusgoalsallowed AS decimal(10,2)) - MIN(PSxGminusgoalsallowed) OVER()) / NULLIF(MAX(PSxGminusgoalsallowed) OVER() - MIN(PSxGminusgoalsallowed) OVER(), 0) as decimal(6,3)) as norm_PSxGminusgoalsallowed,
+	(CAST(crossesstopped AS float) - MAX(crossesstopped) OVER())/ NULLIF(MIN(crossesstopped) OVER () - MAX(crossesstopped) OVER(), 0) AS norm_crossesstopped,
+	CAST((CAST(crossstopperc AS decimal(10,2)) - MIN(crossstopperc) OVER()) / NULLIF(MAX(crossstopperc) OVER() - MIN(crossstopperc) OVER(), 0) as decimal(6,3)) as norm_crossstopperc,
+	CAST((CAST(defactionoutsidepen90 AS decimal(10,2)) - MIN(defactionoutsidepen90) OVER()) / NULLIF(MAX(defactionoutsidepen90) OVER() - MIN(defactionoutsidepen90) OVER(), 0) as decimal(6,3)) as norm_defactionoutsidepen90
 FROM Filtered)
 
 SELECT * FROM normalized
