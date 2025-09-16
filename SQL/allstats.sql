3a492e26626d00fa82e47ed6f22c7518bab5b64f
Brandon Dyer-Smith <brandon.dyer-smith@nologostudios.com>
2025-09-16 16:58:51 +0200

    Commented in Fan tots

--- SQL/allstats.sql
+++ SQL/allstats.sql
@@ -1,2 +1,19 @@
 SELECT *
   FROM [dbo].[SoccerPlayerStats]
+
+  -- Fans TOTS
+
+  --Mats Sels GK
+
+  -- Daniel Munoz
+  -- William Saliba
+  -- Virgil Van Dijk
+  -- Milos Kerkez
+
+  --Mohammed Salah
+  --Ryan Gravenberch
+  --Declan Rice
+  --Bryan Mbuembo
+
+  --Alexander Isak
+  --Chris Wood
