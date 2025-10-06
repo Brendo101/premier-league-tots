8e348155850fd988d26bbf48b97a523698a45a3b
Brandon Dyer-Smith <brandon.dyer-smith@nologostudios.com>
2025-10-06 12:33:44 +0200

    Completed Forwards

--- SQL/FW Stat Normalisation and Analysis.sql
+++ SQL/FW Stat Normalisation and Analysis.sql
@@ -36,9 +36,9 @@ AlteredToRate AS(
 		CAST(TakeOnSucc as decimal(10,4)) / NULLIF(Minutes,0) as TakeOnSuccpermin,
 		CAST(Dispossessed as decimal(10,4)) / NULLIF(Minutes,0) as Dispossessedpermin,
 		CAST(Miscontrols as decimal(10,4)) / NULLIF(Minutes,0) as Miscontrolspermin
-	FROM Filtered)
+	FROM Filtered),
 
-Select 
+normalised as (Select 
 	TOTS,
 	PremFanTots,
 	Player,
@@ -47,6 +47,14 @@ Select
 	Minutes,
 	MP,
 	Nineties,
+	nonpengoals90,
+	xpectedGoals,
+	npxG90,
+	gls90,
+	ast90,
+	xpectedAssistedGoals,
+	xAG90,
+	TakeOnSuccPercen,
 	CAST((nonpengoalspermin - MIN(nonpengoalspermin) OVER()) / NULLIF(MAX(nonpengoalspermin) OVER() - MIN(nonpengoalspermin) OVER(), 0) as Decimal(10,4)) as norm_nonpengoalspermin,
 	CAST((astpermin - MIN(astpermin) OVER()) / NULLIF(MAX(astpermin) OVER() - MIN(astpermin) OVER(), 0) as Decimal(10,4)) as norm_astpermin,
 	CAST((shotcreatingactionspermin - MIN(shotcreatingactionspermin) OVER()) / NULLIF(MAX(shotcreatingactionspermin) OVER() - MIN(shotcreatingactionspermin) OVER(), 0) as Decimal(10,4)) as norm_shotcreatingactionspermin,
@@ -57,5 +65,45 @@ Select
 	CAST((Touchespermin - MIN(Touchespermin) OVER()) / NULLIF(MAX(Touchespermin) OVER() - MIN(Touchespermin) OVER(), 0) as Decimal(10,4)) as norm_Touchespermin,
 	CAST((Carriespermin - MIN(Carriespermin) OVER()) / NULLIF(MAX(Carriespermin) OVER() - MIN(Carriespermin) OVER(), 0) as Decimal(10,4)) as norm_Carriespermin,
 	CAST((TakeOnAttemptedpermin - MIN(TakeOnAttemptedpermin) OVER()) / NULLIF(MAX(TakeOnAttemptedpermin) OVER() - MIN(TakeOnAttemptedpermin) OVER(), 0) as Decimal(10,4)) as norm_TakeOnAttemptedpermin,
-	CAST((TakeOnSuccpermin - MIN(TakeOnSuccpermin) OVER()) / NULLIF(MAX(TakeOnSuccpermin) OVER() - MIN(TakeOnSuccpermin) OVER(), 0) as Decimal(10,4)) as norm_TakeOnSuccpermin
-From AlteredToRate
+	CAST((TakeOnSuccpermin - MIN(TakeOnSuccpermin) OVER()) / NULLIF(MAX(TakeOnSuccpermin) OVER() - MIN(TakeOnSuccpermin) OVER(), 0) as Decimal(10,4)) as norm_TakeOnSuccpermin,
+	CAST((Dispossessedpermin - MAX(Dispossessedpermin) OVER ()) / NULLIF(MIN(Dispossessedpermin) OVER () - MAX(Dispossessedpermin) OVER(), 0) as Decimal(10,4)) as norm_Dispossessedpermin,
+	CAST((Miscontrolspermin - MAX(Miscontrolspermin) OVER ()) / NULLIF(MIN(Miscontrolspermin) OVER () - MAX(Miscontrolspermin) OVER(), 0) as Decimal(10,4)) as norm_Miscontrolspermin
+From AlteredToRate)
+
+Select 
+	TOTS,
+	PremFanTots,
+	Player,
+	Squad,
+	Pos1,
+	Minutes,
+	MP,
+	Nineties,
+	CAST(
+  0.12 * norm_nonpengoalspermin +
+  0.10 * npxG90 +
+  0.08 * gls90 +
+  0.07 * xpectedgoals +
+  0.08 * xAG90 +
+  0.07 * xpectedassistedgoals +
+  0.06 * norm_astpermin +
+  0.07 * norm_shotcreatingactionspermin +
+  0.05 * norm_glcreatingactionpermin +
+  0.05 * norm_progcarriespermin +
+  0.03 * norm_progpassespermin +
+  0.04 * norm_ProgPassesReceivedpermin +
+  0.03 * norm_touchespermin +
+  0.02 * norm_takeonattemptedpermin +
+  0.03 * norm_takeonsuccpermin +
+  0.06 * norm_Dispossessedpermin +
+  0.04 * norm_miscontrolspermin
+AS decimal(10,4)) AS CompositeScore
+
+
+From normalised
+ORDER BY CompositeScore DESC
+
+-- Split into Finishing 45%
+-- Creation 25%
+-- Progression 20%
+-- Retention 10%
