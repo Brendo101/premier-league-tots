60439a247473f705d2859578b495fa66b8309560
Brandon Dyer-Smith <brandon.dyer-smith@nologostudios.com>
2025-10-10 08:01:40 +0200

    Carried on with Power BI and Finished SQL

--- SQL/Squad Stats.sql
+++ SQL/Squad Stats.sql
@@ -8,10 +8,17 @@ SELECT
     COUNT(CASE WHEN Pos1 = 'MF' THEN 1 END) AS Midfielders,
     COUNT(CASE WHEN Pos1 = 'DF' THEN 1 END) AS Defenders,
     COUNT(CASE WHEN Pos1 = 'GK' THEN 1 END) AS Goalkeepers,
+    COUNT(CASE WHEN TOTS = 'Yes' THEN 1 END) AS TOTS_Count,
+    COUNT(CASE WHEN PremFanTOTS = 'Yes' THEN 1 END) AS FanTOTS_Count,
+   CAST(AVG(CASE WHEN Pos1 = 'FW' THEN CAST(Gls AS decimal(10,2)) END) AS decimal(10,2)) AS Avg_Goals_FW,
+    CAST(AVG(CASE WHEN Pos1 = 'MF' THEN CAST(Gls AS decimal(10,2)) END) AS decimal(10,2)) AS Avg_Goals_MF,
+    CAST(AVG(CASE WHEN Pos1 = 'DF' THEN CAST(Gls AS decimal(10,2)) END) AS decimal(10,2)) AS Avg_Goals_DF,
+    CAST(AVG(CASE WHEN Pos1 = 'GK' THEN CAST(Gls AS decimal(10,2)) END) AS decimal(10,2)) AS Avg_Goals_GK,
     MAX(Age) AS Oldest_Player,
     Min(Age) AS Youngest_Player,
     SUM(pensattempted) as Penalties_Awarded,
     SUM(pensscored) as Penalties_Scored,
-    SUM(minutes) as Minutes_Played
+    SUM(minutes) as Minutes_Played,
+    CAST(SUM(Minutes) * 1.0 / COUNT(Player) AS decimal(10,2)) AS Avg_Minutes_Per_Player
 FROM SoccerPlayerStats
 GROUP BY Squad;
